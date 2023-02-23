15d791f88b202e36a5de1546996c144e349b9f20
Godfrey <mnoelgody7@gmail.com>
2023-02-22 19:11:59 -0800

    Task 17

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,43 @@
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_fibonacci(int n);
+
+#endif /* MAIN_H */
+
+/* 104-fibonacci.c */
 #include "main.h"
 
 /**
- * print_fibonacci - Prints the first 98 Fibonacci numbers
+ * print_fibonacci - prints the first n Fibonacci numbers
+ * @n: the number of Fibonacci numbers to print
  */
-void print_fibonacci(void)
+void print_fibonacci(int n)
 {
-    unsigned long int i, num1 = 1, num2 = 2, next;
+    int i;
+    unsigned long int a = 1, b = 2, c;
 
-    _putchar('1');
-    _putchar(',');
-    _putchar(' ');
-    _putchar('2');
-    _putchar(',');
-    _putchar(' ');
-
-    for (i = 3; i <= 98; i++)
-    {
-        next = num1 + num2;
-        _putchar(next + '0');
-
-        if (i != 98)
-        {
+    if (n >= 1)
+        _putchar('1');
+    if (n >= 2)
+        _putchar(',');
+        _putchar(' ');
+    for (i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+        printf("%lu", c);
+        if (i != n)
             _putchar(',');
             _putchar(' ');
-        }
-
-        num1 = num2;
-        num2 = next;
     }
-
     _putchar('\n');
+}
+
+int main(void)
+{
+    print_fibonacci(98);
+    return (0);
 }
