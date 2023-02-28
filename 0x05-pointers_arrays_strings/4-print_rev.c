99e3ea88a3a62b001a4bf975c5a2be7a97ae1061
G-Noel <mnoelgody7@gmail.com>
2023-02-28 04:33:59 -0800

    Change 05

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,15 @@
 void print_rev(char *s)
 {
 	int len = 0;
-
-	while (*(s + len) != '\0')
+	while (s[len] != '\0')
+	{
 		len++;
+	}
 
 	for (int i = len - 1; i >= 0; i--)
-		_putchar(*(s + i));
+	{
+		_putchar(s[i]);
+	}
 
 	_putchar('\n');
 }
