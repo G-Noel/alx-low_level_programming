0b70a457a285cb9230cb534497b8a7d37e72b623
G-Noel <mnoelgody7@gmail.com>
2023-02-28 18:10:42 -0800

    Add solution for task 6

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int array[5] = {98, 402, -198, 298, -1024};
+	int n = 5;
+
+	print_array(array, n);
+	return (0);
+}
