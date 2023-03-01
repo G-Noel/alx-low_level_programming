5217dd334e26a79d2f37ff5cf6549c1251d73ef7
G-Noel <mnoelgody7@gmail.com>
2023-02-28 18:26:29 -0800

    Implement _strcpy function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src, including the null byte, 
- * to the buffer pointed to by dest.
- * @dest: A pointer to the buffer.
- * @src: The string to copy.
+ * _strcpy - copy string pointed to by src, including null byte, to buffer
+ * @dest: buffer to copy string into
+ * @src: string to copy
  *
- * Return: A pointer to the destination string dest.
+ * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	while (src[i])
+	i = 0;
+	while (*(src + i))
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
 		i++;
 	}
-
-	dest[i] = '\0';
-
+	*(dest + i) = '\0';
 	return (dest);
 }
